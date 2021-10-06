8a1d049773bc91c1dfec5a4e4bcbbb034ee5c7f0
Rutvik1106 <rutvikpateldsrp@gmail.com>
2021-10-06 12:57:37 +0530

    Login and signup

--- client/src/pages/Auth/Login/Login.js
+++ client/src/pages/Auth/Login/Login.js
@@ -1,91 +1,115 @@
-import { useContext, useState } from 'react'
-import { Link,useHistory } from "react-router-dom"
-import { motion } from 'framer-motion'
-import { Context } from '../../../utils/context'
-import api from '../../../utils/api'
-import { PageAnimation } from '../../../utils/PageAnimation'
-import PasswordInput from '../Components/PasswordInput'
-import { LoginValidation } from '../Utils/Validation'
-import { SweetError, SweetInfo, SweetWait, SweetWelcome, SweetWrong } from '../../../SweetAlert'
-
+import { useContext, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { motion } from "framer-motion";
+import { Context } from "../../../utils/context";
+import api from "../../../utils/api";
+import { PageAnimation } from "../../../utils/PageAnimation";
+import PasswordInput from "../Components/PasswordInput";
+import { LoginValidation } from "../Utils/Validation";
+import {
+  SweetError,
+  SweetInfo,
+  SweetWait,
+  SweetWelcome,
+  SweetWrong,
+} from "../../../SweetAlert";
+import signinimage from "../../../SvgIcons/SignIn.svg";
+import "../style.css";
 
 const Login = () => {
+  const [state, dispatch] = useContext(Context);
 
-    const [state, dispatch] = useContext(Context)
+  const history = useHistory();
 
-    const history = useHistory()
+  const [UserDetails, setUserDetails] = useState({
+    username: "",
+    password: "",
+  });
 
-    const [UserDetails, setUserDetails] = useState({
-        username:'',
-        password:''
-    })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+    if (LoginValidation(UserDetails)) {
+      try {
+        SweetWait();
 
-        if(LoginValidation(UserDetails)) {
-            try { 
+        const res = await api.post("/login", UserDetails);
+        const { success, info, error, user, token } = res.data;
 
-                SweetWait()
-
-                const res = await api.post('/login',UserDetails)
-                const { success , info , error ,user,token } = res.data  
-                
-                if(success){
-                    SweetWelcome(success)
-                    dispatch({type:'SET_USER',user,token})
-                    history.push('/orders')
-                }
-                else if(info)   
-                    SweetInfo(info)
-                else    
-                    SweetError(error)     
-            }catch (error){ 
-                SweetWrong() 
-            }           
-        }
+        if (success) {
+          SweetWelcome(success);
+          dispatch({ type: "SET_USER", user, token });
+          history.push("/orders");
+        } else if (info) SweetInfo(info);
+        else SweetError(error);
+      } catch (error) {
+        SweetWrong();
+      }
     }
+  };
 
-    const handleChange = (e) => {
-        const {name ,value } = e.target
-        setUserDetails((prev) => {
-            return { ...prev,[name]:value }
-        })
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserDetails((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
 
-    return (
-        <motion.section className="flex justify-center pt-24" initial='in' animate='out' exit='exit' 
-                        variants={PageAnimation} transition={{ duration: 0.4 }}>
-            <div className="w-full max-w-xs">
-                <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label className="input-label" htmlFor="username">Email</label>
-                    <input 
-                        name="username" 
-                        value={UserDetails.username} 
-                        onChange={handleChange}
-                        className="input-box"
-                        type="text" 
-                        placeholder="Enter your Email" />
-                </div>
-                <PasswordInput
-                    value={UserDetails.password} 
-                    onChange={handleChange}
-                />
-                <Link className="primary-text align-baseline font-bold text-sm" to="/forgot">
-                    Forgot Password?
-                </Link>
-                <button className="btn-primary btn mt-3" type="submit">Log In</button>
-                <div className="w-full text-center my-4">
-                    <Link className="primary-text align-baseline font-bold text-sm" to="/signup">
-                        Don't have account?
-                    </Link>
-                </div>
-                </form>
-                <p className="text-center text-gray-500 text-xs">&copy;2021 FoodEasy.  All rights reserved.</p>
-            </div>
-        </motion.section>
-    )
-}
+  return (
+    <motion.section
+      className="flex justify-center pt-24"
+      initial="in"
+      animate="out"
+      exit="exit"
+      variants={PageAnimation}
+      transition={{ duration: 0.4 }}
+    >
+      
+        <img src={signinimage} height="30%" width="20%" />
+      
+      <div className="w-full max-w-xs">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label className="input-label" htmlFor="username">
+              Email
+            </label>
+            <input
+              name="username"
+              value={UserDetails.username}
+              onChange={handleChange}
+              className="input-box"
+              type="text"
+              placeholder="Enter your Email"
+            />
+          </div>
+          <PasswordInput value={UserDetails.password} onChange={handleChange} />
+          <Link
+            className="primary-text align-baseline font-bold text-sm"
+            to="/forgot"
+          >
+            Forgot Password?
+          </Link>
+          <button className="btn-primary btn mt-3" type="submit">
+            Log In
+          </button>
+          <div className="w-full text-center my-4">
+            <Link
+              className="primary-text align-baseline font-bold text-sm"
+              to="/signup"
+            >
+              Don't have account?
+            </Link>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+          &copy;2021 FoodEasy. All rights reserved.
+        </p>
+      </div>
+    </motion.section>
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Auth/Registration/Register.js
+++ client/src/pages/Auth/Registration/Register.js
@@ -1,145 +1,168 @@
-import React, { useState } from 'react'
-import { Link, useHistory } from "react-router-dom"
-import swal from 'sweetalert'
-import { motion } from 'framer-motion'
-import PasswordInput from '../Components/PasswordInput'
-import { RegiterValidation } from '../Utils/Validation'
-import { SweetError, SweetInfo, SweetOtpConfig, SweetOtpWrong, SweetSuccess, SweetWait, SweetWrong } from '../../../SweetAlert'
-import { PageAnimation } from '../../../utils/PageAnimation'
-import api from '../../../utils/api'
-import { SendEmail } from '../../../EmailJs/SendEmail'
-import { VerifyOtpTemplate } from '../../../EmailJs/Templates'
-import { generateOTP } from '../../../utils/functions'
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import swal from "sweetalert";
+import { motion } from "framer-motion";
+import PasswordInput from "../Components/PasswordInput";
+import { RegiterValidation } from "../Utils/Validation";
+import {
+  SweetError,
+  SweetInfo,
+  SweetOtpConfig,
+  SweetOtpWrong,
+  SweetSuccess,
+  SweetWait,
+  SweetWrong,
+} from "../../../SweetAlert";
+import { PageAnimation } from "../../../utils/PageAnimation";
+import api from "../../../utils/api";
+import { SendEmail } from "../../../EmailJs/SendEmail";
+import { VerifyOtpTemplate } from "../../../EmailJs/Templates";
+import { generateOTP } from "../../../utils/functions";
+import signupimage from "../../../SvgIcons/SignUp.svg";
 
 const Register = () => {
+  const [UserDetails, setUserDetails] = useState({
+    username: "",
+    email: "",
+    password: "",
+    phone: "",
+  });
 
-    const [UserDetails, setUserDetails] = useState({
-        username:'',
-        email:'',
-        password:'',
-        phone:''
-    })
-    
-    const [otpStatusText, setOtpStatusText] = useState('Send Otp')
+  const [otpStatusText, setOtpStatusText] = useState("Send Otp");
 
-    const history = useHistory()
+  const history = useHistory();
 
-    const [otp, setOtp] = useState(generateOTP())
+  const [otp, setOtp] = useState(generateOTP());
 
-    const handleSubmit = async (e) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        e.preventDefault() 
+    if (RegiterValidation(UserDetails)) {
+      SweetWait();
 
-        if(RegiterValidation(UserDetails)){
-           
-            SweetWait()
+      const res = await api.post("/userexist", UserDetails.email);
 
-            const res = await api.post('/userexist',UserDetails.email)
-    
-            const { info , error  } = res.data  
+      const { info, error } = res.data;
 
-            if(info){
-                SweetInfo(info)
-                history.push('/signin')
-            }
-            else if(error)
-                SweetWrong()
-            else{
-        
-            const sendEmail = {
-                otp,
-                username:UserDetails.username,
-                email:UserDetails.email
-            }
-            try {
-                
-                const isSent  = await SendEmail(VerifyOtpTemplate(sendEmail))
-                
-                if(isSent.status === 200){
-                   
-                    const userOtp = await swal(SweetOtpConfig)
-                    if(otp === userOtp){
+      if (info) {
+        SweetInfo(info);
+        history.push("/signin");
+      } else if (error) SweetWrong();
+      else {
+        const sendEmail = {
+          otp,
+          username: UserDetails.username,
+          email: UserDetails.email,
+        };
+        try {
+          const isSent = await SendEmail(VerifyOtpTemplate(sendEmail));
 
-                        swal.stopLoading()
-                        swal.close()
+          if (isSent.status === 200) {
+            const userOtp = await swal(SweetOtpConfig);
+            if (otp === userOtp) {
+              swal.stopLoading();
+              swal.close();
 
-                        const res = await api.post('/register',UserDetails)
-                        const { success , error } = res.data
-            
-                        if(success){
-                            SweetSuccess(success)
-                            history.push('/signin')
-                        }
-                        else
-                            SweetError(error)          
-                    }else
-                        SweetOtpWrong() 
-                        setOtpStatusText('Resend Otp')   
-                }
-            } catch (error) {
-                SweetWrong()
-            }
+              const res = await api.post("/register", UserDetails);
+              const { success, error } = res.data;
+
+              if (success) {
+                SweetSuccess(success);
+                history.push("/signin");
+              } else SweetError(error);
+            } else SweetOtpWrong();
+            setOtpStatusText("Resend Otp");
+          }
+        } catch (error) {
+          SweetWrong();
         }
+      }
     }
-}
+  };
 
-    const handleChange = (e) => {
-        const {name ,value } = e.target
-        setUserDetails((prev) => {
-            return { ...prev,[name]:value }
-        })
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserDetails((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
 
-    return (
-        <motion.section className="flex justify-center pt-20" initial='in' animate='out' exit='exit' 
-                        variants={PageAnimation} transition={{ duration: 0.4 }}>
-            <div className="w-full max-w-xs">
-                <form onSubmit={handleSubmit} autoComplete="new-password" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                    <div className="mb-4">
-                        <label className="input-label" htmlFor="username">Name</label>
-                        <input 
-                            name="username" 
-                            value={UserDetails.username} 
-                            onChange={handleChange}
-                            className="input-box"
-                            type="text" 
-                            placeholder="Enter your name" 
-                            autoComplete="none" />
-                    </div>
-                    <div className="mb-4">
-                        <label className="input-label" htmlFor="email">Email</label>
-                        <input 
-                            name="email" 
-                            value={UserDetails.email} 
-                            onChange={handleChange}
-                            className="input-box" autoComplete="none"
-                            type="email" 
-                            placeholder="Enter your email" />
-                    </div>
-                    <div className="mb-4">
-                        <label className="input-label" htmlFor="phone">Phone</label>
-                        <input 
-                            name="phone" 
-                            value={UserDetails.phone} 
-                            onChange={handleChange}
-                            className="input-box"
-                            placeholder="Enter your phone number" 
-                            autoComplete="none" />
-                    </div>
-                    <PasswordInput 
-                        value={UserDetails.password} 
-                        onChange={handleChange} />
-                    <button className="btn-primary btn mt-4" type="submit">{otpStatusText}</button>
-                    <div className="w-full text-center my-4">
-                        <Link className="primary-text align-baseline font-bold text-sm" to="/signin">
-                            Already have account?
-                        </Link>
-                    </div>
-                </form>
-                <p className="text-center text-gray-500 text-xs">&copy;2021 FoodEasy.All rights reserved.</p>
-            </div>
-        </motion.section>
-    )
-}
+  return (
+    <motion.section
+      className="flex justify-center pt-20"
+      initial="in"
+      animate="out"
+      exit="exit"
+      variants={PageAnimation}
+      transition={{ duration: 0.4 }}
+    >
+      <img src={signupimage} height="30%" width="30%" />
+      <div className="w-full max-w-xs">
+        <form
+          onSubmit={handleSubmit}
+          autoComplete="new-password"
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <label className="input-label" htmlFor="username">
+              Name
+            </label>
+            <input
+              name="username"
+              value={UserDetails.username}
+              onChange={handleChange}
+              className="input-box"
+              type="text"
+              placeholder="Enter your name"
+              autoComplete="none"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="input-label" htmlFor="email">
+              Email
+            </label>
+            <input
+              name="email"
+              value={UserDetails.email}
+              onChange={handleChange}
+              className="input-box"
+              autoComplete="none"
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="input-label" htmlFor="phone">
+              Phone
+            </label>
+            <input
+              name="phone"
+              value={UserDetails.phone}
+              onChange={handleChange}
+              className="input-box"
+              placeholder="Enter your phone number"
+              autoComplete="none"
+            />
+          </div>
+          <PasswordInput value={UserDetails.password} onChange={handleChange} />
+          <button className="btn-primary btn mt-4" type="submit">
+            {otpStatusText}
+          </button>
+          <div className="w-full text-center my-4">
+            <Link
+              className="primary-text align-baseline font-bold text-sm"
+              to="/signin"
+            >
+              Already have account?
+            </Link>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs">
+          &copy;2021 FoodEasy.All rights reserved.
+        </p>
+      </div>
+    </motion.section>
+  );
+};
 
-export default Register
+export default Register;
