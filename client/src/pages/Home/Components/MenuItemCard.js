467ef47c889069086e5ae89567f20246b3278db3
Rutvik1106 <rutvikpateldsrp@gmail.com>
2021-10-06 12:01:38 +0530

    Menu and Profile

--- client/src/pages/Home/Components/MenuItemCard.js
+++ client/src/pages/Home/Components/MenuItemCard.js
@@ -8,17 +8,21 @@ const MenuItemCard = ({item}) => {
     const [{}, dispatch] = useContext(Context)
 
     return (
-        <div className="my-3 card-ipad px-5 w-full md:w-1/2 lg:my-4 md:mx-12 lg:px-4 lg:w-1/4">
-            <article className="bg-white overflow-hidden rounded-lg shadow-lg">
-                <img alt={item.title} className="block h-auto w-full" src={item.image} />
-                <header className="flex items-center flexible justify-center leading-tight p-2 md:p-4">
-                    <h1 className="text-lg">{item.title}</h1>
-                </header>
-                <footer className="flex items-center justify-between leading-none p-2">
-                    <h2>
-                        <span className="font-bold">Price : </span>
-                        <span>{ item.price } ₹</span>
-                    </h2>
+        <div className="wrapper bg-gray antialiased text-gray-900 m-5">
+            <div>
+            <article className="bg-white overflow-hidden rounded-lg shadow-lg ">
+                <img alt={item.title} className="w-full object-cover object-center rounded-lg shadow-md" src={item.image} />
+                <div class="relative px-6 -mt-16 m-5 ">
+                <div class ="bg-white p-6 rounded-lg shadow-lg">
+                <div class ="flex items-baseline">
+                <footer className="p-1.5">
+                <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">{item.title}</h4>
+                            
+                <div class="mt-1">
+                Price : 
+                <span class ="text-gray-600 text-sm"> { item.price } ₹</span>
+                </div>
+                
                     <button onClick={() => {
                             toast.success('Item Added To Cart Succesfully', {
                                 position: "top-right",
@@ -31,13 +35,19 @@ const MenuItemCard = ({item}) => {
                                 })
                             dispatch({type:"ADD_CART",item})
                             }} 
-                            className="no-underline focus:outline-none card-button rounded-md p-2">
+                                        className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-5 rounded-full m-4 mb-0">
                         <span>
-                            <CartIcon stroke="#fff" classes="h-6 w-6" />
+                            Add to Cart
                         </span>
+                        
                     </button>
-                </footer>
-            </article>
+                                </footer>
+                            
+                        </div>
+                        </div>
+                </div>
+                </article>
+            </div>
         </div>
     )
 }
--- client/src/pages/MyProfile/MyProfile.js
+++ client/src/pages/MyProfile/MyProfile.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom'
 import swal from 'sweetalert'
 import { Context } from '../../utils/context'
 import { PageAnimation } from '../../utils/PageAnimation'
+import profile from '../../SvgIcons/ProfileImage.svg'
 
 const MyProfile = () => {
 
@@ -30,41 +31,34 @@ const MyProfile = () => {
     
     return (
         <motion.section className="flex justify-center pt-24" initial='in' animate='out' exit='exit' 
-                        variants={PageAnimation} transition={{ duration: 0.4 }}>
-            <div className="w-full mx-3 md:w-1/2">
-                <div className="bg-white shadow-md rounded px-4 md:px-8 pt-6 pb-8 mb-4">
-                    <div className="flex justify-center shadow-md border-2 border-gray-800">
-                        <h1 className="text-2xl font-bold primary-text">My Profile</h1>
-                    </div>
-                    <div className="grid mt-7 grid-cols-2 gap-3 md:gap-5 border-b-2 pb-5">
-                        <p className="flex justify-between">
-                            <span className="capitalize font-bold text-base md:text-lg primary-text">Name</span>
-                            <span>:</span>
-                        </p>
-                        <p className="text-lg text-gray-600 break-words">{state.user.username || 'NA'}</p>
-                        <p className="flex justify-between">
-                            <span className="capitalize font-bold text-base md:text-lg primary-text">Email</span>
-                            <span>:</span>
-                        </p>
-                        <p className="text-base text-gray-600 md:text-lg break-words">{state.user.email || 'NA'}</p>
-                        <p className="flex justify-between">
-                            <span className="capitalize font-bold text-base md:text-lg primary-text">Phone</span>
-                            <span>:</span>
-                        </p>
-                        <p className="text-base text-gray-600 md:text-lg break-words">{state.user.phone || 'NA'}</p>
-                    </div>
-                    <div className="w-full text-center mt-2">
-                        <button onClick={logout} className="navlink rounded-md primary-text">
+            variants={PageAnimation} transition={{ duration: 0.4 }}>
+            
+            <div className="w-full lg:w-2/5 rounded-lg shadow-xl bg-white opacity-75" style={{ marginTop:"-60px"}}>
+                <div class="w-full lg:w-2/5 bg-white mx-44 lg:mx-30 my-0">
+                    <img src={profile} alt="profile" class="rounded-none lg:rounded-lg hidden lg:block h-70 mb-1" />
+                </div>
+                <div className="p-4 md:p-12 text-center lg:text-left mt-0">
+                    
+                    <h1 className="text-3xl font-bold pt-8 lg:pt-0 mt-0">{state.user.username || 'NA'}</h1>
+                    <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
+                    <p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" /></svg>{state.user.email || 'NA'}</p>
+                    <p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" /></svg> {state.user.phone || 'NA'}</p>
+                    
+                    <div className="pt-12 pb-8">
+                        <button onClick={logout} className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
                             <span className="tracking-wider">Logout</span>
                         </button>
-                    </div>
-                    <div className="w-full text-center mt-4">
-                        <button onClick={() => history.push('/contact')} className="navlink rounded-md primary-text" to="/editprofile">
+
+                        <button onClick={() => history.push('/contact')} className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full mx-1" to="/editprofile">
                             <span className="tracking-wider">Contact Us</span>
                         </button>
-                    </div>
+                    </div>                   
                 </div>
             </div>
+
+            
+
+           
         </motion.section>
     )
 }
